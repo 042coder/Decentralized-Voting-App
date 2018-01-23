e1b249138425faf53c5064e38e6b319dfd10a502
tapishr <tapish@iitj.ac.in>
2018-01-23 10:11:10 -0800

    docs: Add comments to explain eth API

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
+//web3 object for accessing ethereeum APIs
 web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+//abi --> containes interface of Voting class in Json form
 abi = JSON.parse('[{"constant":true,"inputs":[{"name":"","type":"bytes32"}],"name":"votes","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[{"name":"","type":"uint256"}],"name":"candidates","outputs":[{"name":"","type":"bytes32"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"IncrementVote","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"CandidateExists","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"candidate","type":"bytes32"}],"name":"NumVotes","outputs":[{"name":"","type":"uint8"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[{"name":"names","type":"bytes32[]"}],"payable":false,"stateMutability":"nonpayable","type":"constructor"}]')
+//The contract in evm
 VotingContract = web3.eth.contract(abi);
+//Instance of contract inside evm
 contractInstance = VotingContract.at('0x3e22ae8404a1fb45473c04c9062b3afc90d5cd82')
 candidates = {"Rama": "candidate-1", "Nick": "candidate-2", "Jose": "candidate-3"};
 
+//increment vote for candidate by invoking contract
 function voteForCandidate() {
   candidateName = $("#candidate").val();
   contractInstance.IncrementVote(candidateName, {from: web3.eth.accounts[0]}, function() {
@@ -12,6 +17,7 @@ function voteForCandidate() {
   });
 }
 
+//display names and votes
 $(document).ready(function() {
   candidateNames = Object.keys(candidates);
   for (var i = 0; i < candidateNames.length; i++) {
